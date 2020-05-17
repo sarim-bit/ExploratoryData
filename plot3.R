cf9cbcd5723272bc07d200bb337d2c4e5f460562
sarim-bit <sarimrizvi41@gmail.com>
2020-05-17 21:41:26 +0530

    uncommented lines

--- plot3.R
+++ plot3.R
@@ -1,8 +1,8 @@
-# data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE)
-# # Set date format
-# data[,"Date"] <- as.Date(data[,"Date"],format = "%d/%m/%Y")
-# # Filter data
-# data_n<-subset(data,Date == "2007-02-01" | Date == "2007-02-02")
+data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE)
+# Set date format
+data[,"Date"] <- as.Date(data[,"Date"],format = "%d/%m/%Y")
+# Filter data
+data_n<-subset(data,Date == "2007-02-01" | Date == "2007-02-02")
 # Date-time format
 date_time<-strptime(paste(sdata$Date,sdata$Time,sep = " "), "%Y-%m-%d %H:%M:%S" )
 # Convert string to numeric format
